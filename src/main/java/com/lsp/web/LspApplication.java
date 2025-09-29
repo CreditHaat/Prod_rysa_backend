5b5187508a1d91ed83b974bef52460d82fcfc5bf
CreditHaat <rahulgupta.credithaat@gm1il.com>
2025-09-29 11:55:05 +0530

    datetime from utc to Asia/Kolkata

--- src/main/java/com/lsp/web/LspApplication.java
+++ src/main/java/com/lsp/web/LspApplication.java
@@ -2,11 +2,13 @@ package com.lsp.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class LspApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(LspApplication.class, args);
 	}
 
--- src/main/java/com/lsp/web/ServletInitializer.java
+++ src/main/java/com/lsp/web/ServletInitializer.java
@@ -2,11 +2,13 @@ package com.lsp.web;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import java.util.TimeZone;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		return application.sources(LspApplication.class);
 	}
 
